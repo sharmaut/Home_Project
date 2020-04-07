4f5827f70be0e2a66ec35471e8637cdd7f45905c
sharmaut <40994950+sharmaut@users.noreply.github.com>
2020-04-07 15:24:48 +1000

    Add files via upload

---
+++ tgv.js
@@ -0,0 +1,12 @@
+function projectJS())
+{
+
+var b = document.getElementById('Project Name').value
+var b = document.getElementById('Project Manager').value
+var b = document.getElementById('Project Start Date').value
+var b = document.getElementById('Project End Date').value
+var b = document.getElementById('Project Value').value
+
+document.getElementById('here').innerHTML = b;
+
+}
